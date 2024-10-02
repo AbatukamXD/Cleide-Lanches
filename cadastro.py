a27a34ac7ff2c309acba0a93a72c677556156b80
fivn999 <joaonaiff10@gmail.com>
2024-10-02 16:37:26 -0300

    finalizado cardapio

--- cadastro.py
+++ cadastro.py
@@ -1,3 +1,53 @@
+import os
+
+
+    # Salvar variáveis em um arquivo separado por nomes
+def salvar_variaveis(path_file, **kwargs):
+    with open(path_file, 'w') as f:
+     for nome, valor in kwargs.items():
+        f.write(f"{nome}: {valor}\n")
+
+# Carregar variáveis de um arquivo
+def carregar_variaveis(path_file):
+    variaveis = {}
+    with open(path_file, 'r') as f:
+        for line in f:
+            try:
+                chave, valor = line.strip().split(': ')
+                variaveis[chave] = valor
+            except ValueError:
+                return None 
+    return variaveis
+
+def verifica_primeira_vez():
+    match os.path.exists("variaveis.txt"):
+
+        case True:
+            variaveis_carregadas = carregar_variaveis('variaveis.txt')
+            if variaveis_carregadas != None:
+                if len(variaveis_carregadas) > 0:
+                    print(f"seja bem vindo novamente, seus dados são:\n Nome: {variaveis_carregadas['nome']} \n Numero de Telefone:{variaveis_carregadas['telefone']}\n Email: {variaveis_carregadas['email']}")
+                else:
+                    nome_v = input("por favor digite seu nome")
+                    telefone_v = input("por favor digite seu numero de telefone")
+                    email_v = input("por favor digite seu Email")
+
+                    salvar_variaveis('variaveis.txt',nome = nome_v, telefone = telefone_v, email = email_v )
+            else:
+                nome_v = input("por favor digite seu nome")
+                telefone_v = input("por favor digite seu numero de telefone")
+                email_v = input("por favor digite seu Email")
+
+                salvar_variaveis('variaveis.txt',nome = nome_v, telefone = telefone_v, email = email_v )
+        case False:
+                nome_v = input("por favor digite seu nome")
+                telefone_v = input("por favor digite seu numero de telefone")
+                email_v = input("por favor digite seu Email")
+
+                salvar_variaveis('variaveis.txt',nome = nome_v, telefone = telefone_v, email = email_v )
+
+
+
 valor_total_pedido = 0
 lista_carrinho = []
 
@@ -22,7 +72,7 @@ def cardapio():
         if confirmar_pedido(lista_carrinho):
             total = calcular_total(lista_carrinho)
             print(f"Seu pedido foi confirmado e o valor total ficou de: R$ {total:.2f}")
-            lista_carrinho.clear()  
+            lista_carrinho.clear()  # Limpa o carrinho após a confirmação
         else:
             print("Voltando ao menu principal")
 
@@ -121,14 +171,13 @@ def confirmar_pedido(lista_carrinho):
         print("Você escolheu:")
         for item in lista_carrinho:
             print(f"- {item['nome']} (R$ {item['valor']:.2f})")
+        total = calcular_total(lista_carrinho)
+        print(f"Total do pedido: R$ {total:.2f}")
     else:
         print("Você não fez nenhum pedido.")
         return False
 
     confirmar = input("Deseja confirmar o pedido? [s] ou [n]: ")
-    if confirmar == "n":
-        print ("Retornando ao menu principal.")
-        cardapio()
     return confirmar.lower() == "s"
 
 cardapio()
